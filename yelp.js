133f1ddfc18d3237860a23ebf8081553e8feaad4
ntaliequach <natalie.quach03@gmail.com>
2023-05-28 04:39:33 -0700

    fixed yelp.js again

--- yelp.js
+++ yelp.js
@@ -7,17 +7,14 @@ function load(term) {
 
         for(let i = 0; i < businesses.length; i++){
             document.getElementById('display').innerHTML += `
-                <div class="card" style="width: 16rem;">
-
-
-                <h5 class="card-title">${businesses[i].name}</h5>
-                <img src=${businesses[i].image_url}>
-                <div>Rating: ${businesses[i].rating}/5</div>
-                <div>Reviews: ${businesses[i].review_count}</div>
-                <div>Address: ${businesses[i].location.display_address}</div>
-                <div>Phone: ${businesses[i].display_phone}</div>
-
-                </div> 
+            <div class="card" style="width: 40rem; font-family: "PT Serif Caption";">
+            <h5 class="card-title"><b>${businesses[i].name}</b></h5>
+            <img height="150px" width="150px" src=${businesses[i].image_url}>
+            <div>Rating: ${businesses[i].rating}/5</div>
+            <div>Reviews: ${businesses[i].review_count}</div>
+            <div>Address: ${businesses[i].location.display_address}</div>
+            <div>Phone: ${businesses[i].display_phone}</div>
+            </div>  
             `;
         }
     })
