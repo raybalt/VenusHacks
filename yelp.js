5e82a594711f80fc5839ffbbcd9fa21c1037bf51
Rachel Balta <rachelhbalta@gmail.com>
2023-05-28 01:17:47 -0700

    yelp

--- yelp.js
+++ yelp.js
@@ -7,16 +7,13 @@ function load(term) {
 
         for(let i = 0; i < businesses.length; i++){
             document.getElementById('display').innerHTML += `
-                <div class="card" style="width: 16rem;">
-
-
+                <div class="card" style="width: 40rem;">
                     <h5 class="card-title">${businesses[i].name}</h5>
-                    <img src=${businesses[i].image_url}>
+                    <img height="150px" width="150px" src=${businesses[i].image_url}>
                     <div>Rating: ${businesses[i].rating}/5</div>
                     <div>Reviews: ${businesses[i].review_count}</div>
                     <div>Address: ${businesses[i].location.display_address}</div>
                     <div>Phone: ${businesses[i].display_phone}</div>
-
                 </div>
                 
             `;
